3695ead5d68e2e9a790f21544819fc2a41ebfa5d
Andrew Michel <drewmichel33@gmail.com>
2018-02-10 14:49:03 -0800

    Adding javadocs and comments to Point2DTracker and its Driver.

--- Device/src/singular/strategy/tracker/Point2DTracker.java
+++ Device/src/singular/strategy/tracker/Point2DTracker.java
@@ -4,19 +4,40 @@ import java.awt.geom.Point2D;
 
 /**
  * Created by Andrew Michel on 12/16/2017.
+ *
+ * This class contains static methods to receive two 2D points as doubles
+ * or as Point2D objects along with the speed cap of the first parameter and returns
+ * a 2D point that represents the position that the first object can move to
  */
 public final class Point2DTracker
 {
+    // Private constructor prevents an object of this class from being created
     private Point2DTracker(){}
 
+    /**
+     *
+     * @param x1 the x position of the first object
+     * @param y1 the y position of the first object
+     * @param x2 the x position of the second object
+     * @param y2 the y position of the second object
+     * @param cap the distance cap on the first object
+     * @return position a Point2D.Double object that represents the position that the
+     *          first object can move to without exceeding the cap or passing the
+     *          second object
+     */
     public static Point2D.Double track(double x1, double y1, double x2, double y2, double cap)
     {
         Point2D.Double position;
 
+        // Calculates the distance between the first and second objects
         double distance = Math.hypot(x1 - x2, y1 - y2);
 
+        // Calculates the theta (angle) that the first object would move in to reach the second object
         double theta = Math.atan2(y1 - y2, x1 - x2);
 
+        // If the distance to the object is greater than the allowed movement cap,
+        // Calculate and set the return position to the maximum allowed cap based on angle
+        // Else, calculate and set the return position to the distance based on angle
         if(distance > cap)
         {
             position = new Point2D.Double(Math.cos(theta) * cap, Math.sin(theta) * cap);
@@ -29,14 +50,28 @@ public final class Point2DTracker
         return position;
     }
 
+    /**
+     *
+     * @param p1 the first 2D object
+     * @param p2 the second 2D object
+     * @param cap the distance cap on the first object
+     * @return position a Point2D.Double object that represents the position that the
+     *          first object can move to without exceeding the cap or passing the
+     *          second object
+     */
     public static Point2D.Double track(Point2D p1, Point2D p2, double cap)
     {
         Point2D.Double position;
 
+        // Calculates the distance between the first and second objects
         double distance = p1.distance(p2);
 
+        // Calculates the theta (angle) that the first object would move in to reach the second object
         double theta = Math.atan2(p1.getY() - p2.getY(), p1.getX() - p2.getX());
 
+        // If the distance to the object is greater than the allowed movement cap,
+        // Calculate and set the return position to the maximum allowed cap based on angle
+        // Else, calculate and set the return position to the distance based on angle
         if(distance > cap)
         {
             position = new Point2D.Double(Math.cos(theta) * cap, Math.sin(theta) * cap);
--- Device/test/singular/strategy/tracker/driver/Point2DTrackerDriver.java
+++ Device/test/singular/strategy/tracker/driver/Point2DTrackerDriver.java
@@ -14,47 +14,69 @@ import java.util.Random;
 
 /**
  * Created by Andrew Michel on 12/16/2017.
+ *
+ * This class exists to test the tracking performance of the class Point2DTracker.
+ *
+ * This class contains numerous nested classes because reasons.
+ *
+ * This class creates a JFrame window as well as two types of "Planet" object ArrayLists
+ * and a unique "Planet."
+ * The unique "Planet" is known as the leader and follows the user's mouse
+ * The first ArrayList consists of "followers" that each follow the preceding element.
+ * The first "follower" will follow the leader.
+ * The second ArrayList consists of "orbitals" that each rotate around the leader.
  */
 public class Point2DTrackerDriver extends JFrame
 {
+    // Pixel based constant that is used to limit the maximum speed of the "Planets"
+    // Can be increased or decreased to influence the rate of movement of the "Planets"
     public static final double LEADER_MAX_SPEED = .4, FOLLOWER_MAX_SPEED = .25;
+    public static final double ORBITAL_RATE_DIVISOR = 250;
 
-
+    // Declaring instance variables
     private JPanel panel;
     private MouseEvent mousePoint;
 
     //private Ellipse2D.Double leader, follower, orbit;
     //double orbitAngle;
 
+    // Main method
     public static void main(String[] args)
     {
         Point2DTrackerDriver example = new Point2DTrackerDriver();
         example.setVisible(true);
     }
 
+    // Default constructor
     public Point2DTrackerDriver()
     {
+        // Initial setup
         super("Tracker Example");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         setSize(1080,720);
 
+        // Sets fullscreen
         setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         //getContentPane().setBackground(Color.BLACK);
+
+        // Creates the primary PlanetPanel which contains the leader, followers, and orbitals
         panel = new PlanetPanel(50,100,20, 4,0.35,0.15,500, 40);
+
+        // Sets the background color of the PlanetPanel to black, cause space.
         panel.setBackground(Color.BLACK);
 
-        // hides the cursor
+        // Hides the cursor
         panel.setCursor( panel.getToolkit().createCustomCursor(
                 new BufferedImage( 1, 1, BufferedImage.TYPE_INT_ARGB ),
                 new Point(),
                 null ) );
 
+        // Adds the PlanetPanel to the JFrame
         add(panel);
 
-
-
+        // Creates the mouse event that is used to listen for the mouse's position
         mousePoint = new MouseEvent(this, 0,0,0,getWidth() / 2,getHeight() / 2,0,false,0);//new Point2D.Double(MouseInfo.getPointerInfo().getLocation().getX(), MouseInfo.getPointerInfo().getLocation().getY());
 
         //leader = new Ellipse2D.Double(0,0,50,50);
@@ -62,11 +84,15 @@ public class Point2DTrackerDriver extends JFrame
         //orbit = new Ellipse2D.Double(0,0,20,20);
         //orbitAngle = 0;
 
+        // Moves the JFrame to the center of the screen
         setLocationRelativeTo(null);
+
+        // Adds the mouse event to the Planet Panel
         panel.addMouseMotionListener(new MouseDetecter());
         //setVisible(true);
     }
 
+    // Nested class that updates the instance variable mousePoint whenever the mouse moves.
     private class MouseDetecter implements MouseMotionListener
     {
         @Override
@@ -82,32 +108,41 @@ public class Point2DTrackerDriver extends JFrame
         }
     }
 
+    // Nested class that presents a "Planet"
     private static class Planet
     {
+        // Declares instance variables
         public Point2D.Double point;
         public double diameter, orbit, distance, rate;
         public Color color;
 
+        // Default constructor
         public Planet(){}
 
+        // Method that can be called to drawn this object using its Point2D and diameter
         public void drawPlanet(Graphics g)
         {
             g.fillArc((int)point.getX(), (int)point.getY(),(int) diameter, (int)diameter, 0, 360);
         }
 
+        // Method that returns the x center of the Planet by offseting from the origin
         public double getXCenter()
         {
             return point.getX() + diameter / 2;
         }
 
+        // Method that returns the y center of the Planet by offseting from the origin
         public double getYCenter()
         {
             return point.getY() + diameter / 2;
         }
     }
 
+    // Nested class that extends JPanel and contains several instances of Planets
+    // to display them
     private class PlanetPanel extends JPanel
     {
+        // Declares instance variables
         private final Random generator = new Random();
 
         private ArrayList<Planet> followers;
@@ -118,8 +153,29 @@ public class Point2DTrackerDriver extends JFrame
 
         private double diameterLimit, diameterMin, speedCap, distanceMax, distanceMin, speedMin;
 
+        // RGB color based constants that determine the min color as well as the random color range allowed.
+        // Note that the random generator accepts 216 as 0-215, giving 216 possible combinations.
+        // Therefore, the min + range cannot exceed 225.
+        // These values can be altered to affect the color distribution of the planets
         private final int MIN_RED = 40, MIN_GREEN = 40, MIN_BLUE = 40, RED_RANGE = 216, BLUE_RANGE = 216, GREEN_RANGE = 216;
 
+        private final int DEFAULT_LEADER_DIAMETER = 30;
+        private final Color DEFAULT_LEADER_COLOR = Color.RED;
+
+        /**
+         * Parameter constructor that receives the number of followers, the number of orbitals,
+         * the maximum and minimum allowed diameter sizes, the maximum and minimum speed caps,
+         * and the maximum and minimum allowed orbital distances
+         *
+         * @param followerCount number of followers
+         * @param orbitalCount number of orbitals
+         * @param diameterLimit maximum allowed planet diameter
+         * @param diameterMin minimum allowed planet diameter
+         * @param speedCap maximum allowed speed cap
+         * @param speedMin minimum allowed speed cap
+         * @param distanceMax maximum allowed orbital distance
+         * @param distanceMin minimum allowed orbital distance
+         */
         public PlanetPanel(int followerCount, int orbitalCount, double diameterLimit, double diameterMin, double speedCap, double speedMin, double distanceMax, double distanceMin)
         {
             super();
@@ -143,59 +199,92 @@ public class Point2DTrackerDriver extends JFrame
             populateLeader();
         }
 
+        /**
+         * Populates an ArrayList of Planets by using constants, instance variables, and parameters
+         *
+         * @param list ArrayList containing the planets
+         * @param count the number of planets to be inserted into the list
+         * @param canReverse boolean used for orbital population.  If true, allows the planets to rotate in both directions
+         */
         public void populatePlanets(ArrayList<Planet> list, int count, boolean canReverse)
         {
             list.clear();
 
             Planet current;
 
+            // loop start
             for(int i = 0; i < count; i++)
             {
-
+                // Creates a new planet
                 current = new Planet();
 
                 current.point = new Point2D.Double();
+
+                // Generates the planet's colors using constants
                 current.color = new Color(generator.nextInt(RED_RANGE) + MIN_RED, generator.nextInt(GREEN_RANGE) + MIN_GREEN, generator.nextInt(BLUE_RANGE) + MIN_BLUE);
 
+                // Generates the planet's diameter using instance variables
                 current.diameter = (generator.nextDouble() * (diameterLimit - diameterMin)) + diameterMin;
+
+                // Generates the planet's orbit
                 current.orbit = generator.nextDouble() * generator.nextInt();
 
+                // Generates the planet's distance (from the leader) using instance variables
                 current.distance = generator.nextDouble() * (distanceMax - distanceMin) + distanceMin;
+
+                // Generates the planet's rate using instance variables
                 current.rate = generator.nextDouble() * (speedCap - speedMin) + speedMin;
 
+                // If the object can reverse, generates a number to determine if it should reverse its movement rate
                 if(canReverse && generator.nextInt(2) == 0)
                 {
                     current.rate *= -1;
                 }
 
+                // Adds the planet to the list
                 list.add(current);
-            }
+            } // loop end
         }
 
+        /**
+         * Creates the planet leader using constants and lazy hard values
+         */
         public void populateLeader()
         {
             leader = new Planet();
 
-            leader.diameter = 30;
-            leader.color = Color.RED;
+            leader.diameter = DEFAULT_LEADER_DIAMETER;
+            leader.color = DEFAULT_LEADER_COLOR;
             leader.rate = LEADER_MAX_SPEED;
             leader.orbit = 0;
             leader.distance = 0;
             leader.point = new Point2D.Double(540, 360);
         }
 
+        /**
+         * Updates the leader's position
+         */
         public void updateLeader()
         {
-            Point2D.Double p = Point2DTracker.track(mousePoint.getX(), mousePoint.getY(), leader.point.getX() + leader.diameter / 2, leader.point.getY() + leader.diameter / 2,leader.rate);
+            // Creates a new Point2D.Double that represents the distance that the leader can move
+            // by tracking the mouse's position against the leader's center
+            Point2D.Double p = Point2DTracker.track(mousePoint.getX(), mousePoint.getY(), leader.point.getX() + leader.diameter / 2, leader.point.getY() + leader.diameter / 2, leader.rate);
+
+            // Updates the leader's position by the calculated point's x and y values so that the leader will be drawn at the position
             leader.point.setLocation(p.getX() + leader.point.getX(), p.getY() + leader.point.getY());
             //System.err.println(leader.point);
         }
 
+        /**
+         * Updates the "follower" planets
+         */
         public void updateFollowers()
         {
+            // Sets previous planet to leader
             Planet previous = leader;
             Point2D.Double p = null;
 
+            // loop starts
             for(Planet planet : followers)
             {
                 /*
@@ -207,28 +296,41 @@ public class Point2DTrackerDriver extends JFrame
                 }
                 */
 
+                // Creates a new Point2D.Double that represents the distance that the current follower can move
+                // by tracking against the previous planet's center
                 p = Point2DTracker.track(previous.getXCenter(), previous.getYCenter(), planet.getXCenter(), planet.getYCenter(), planet.rate);
 
+                // If the updated position will ccause the planet's diameters to overlap, the planet has its position set so the diameters are touching instead
                 if(Point2D.Double.distance(previous.getXCenter(), previous.getYCenter(), planet.getXCenter(), planet.getYCenter()) > previous.diameter / 2 + planet.diameter / 2)
                 {
                     planet.point.setLocation(planet.point.getX() + p.getX(), planet.point.getY() + p.getY());
                 }
 
+                // Sets previous planet to current
                 previous = planet;
-            }
+            } // loop ends
 
         }
 
+        /**
+         * Updates the "orbital" planets
+         */
         public void updateOrbitals()
         {
+            // start loop
             for(Planet planet: orbitals)
             {
+                // Sets the planet's position after calculating the next location it should move to
                 planet.point.setLocation(Math.cos(planet.orbit) * planet.distance + leader.getXCenter() - planet.diameter, Math.sin(planet.orbit) * planet.distance + leader.getYCenter() - planet.diameter);
 
-                planet.orbit += planet.rate / 250;
-            }
+                // Increments the planet's orbit by its rate divided by a constant
+                planet.orbit += planet.rate / ORBITAL_RATE_DIVISOR;
+            } // loop ends
         }
 
+        /**
+         * Calls all relevant planet update methods
+         */
         public void updatePlanet()
         {
             updateLeader();
@@ -238,13 +340,17 @@ public class Point2DTrackerDriver extends JFrame
             updateFollowers();
         }
 
+        /**
+         * Calls each planet's drawPlanet method.
+         *
+         * @param g Graphics object that can be passed from a canvas.
+         */
         public void updateVisual(Graphics g)
         {
             //System.err.println(mousePoint);
 
             g.setColor(leader.color);
             leader.drawPlanet(g);
-
 
             for(Planet planet : followers)
             {
@@ -258,17 +364,23 @@ public class Point2DTrackerDriver extends JFrame
                 planet.drawPlanet(g);
             }
 
-
             repaint();
         }
 
+        /**
+         * Overrides the default paintComponent. This allows access to drawing the planets each update.
+         *
+         * @param g Graphics object that can be passed from a canvas.
+         */
         @Override
         public void paintComponent(Graphics g)
         {
             super.paintComponent(g);
 
+            // Update's each planet's position
             updatePlanet();
 
+            // Update's each planet's display
             updateVisual(g);
 
             /*
